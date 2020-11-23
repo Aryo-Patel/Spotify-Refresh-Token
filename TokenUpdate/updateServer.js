c2243adbf4aee2524be21efa4a1cf6d96dec7aa0
AryoP <aryopatel@gmail.com>
2020-11-23 16:58:07 -0600

    refresh token working

--- TokenUpdate/updateServer.js
+++ TokenUpdate/updateServer.js
@@ -18,42 +18,65 @@ const redirect_uri = config.get('REDIRECT_URI');
 
 async function main(){
     const client = new MongoClient(MONGO_URI, {useUnifiedTopology: true, useNewUrlParser: true});
-    const returnArray = [];
-    try{
-        await client.connect();
-
-        let userCollection  = await client.db("SpotifyRefresh").collection("users").find()
-        
-        const userArr = await userCollection.toArray();
-
-        userArr.forEach(async (user) => {
-            let userAccessToken = user.access_token;
-            let userRefreshToken = user.refresh_token;
-            let userEmail = user.email;
-
-            var refresh_token = userRefreshToken;
-            var authOptions = {
-              url: 'https://accounts.spotify.com/api/token',
-              headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
-              form: {
-                grant_type: 'refresh_token',
-                refresh_token: refresh_token
-              },
-              json: true
-            };
-            let newAccessToken = await getNewToken(authOptions);
-            console.log(newAccessToken);
-
-            await client.db("SpotifyRefresh").collection("users").updateOne({email: userEmail}, {$set: {access_token: newAccessToken}});
+    let newSongs;
+    return new Promise(async (resolve, reject) => {
+        try{
+            await client.connect();
+    
+            let userCollection  = await client.db("SpotifyRefresh").collection("users").find()
             
-        })
-    }catch(err){
-        console.log(err);
-    }
+            const userArr = await userCollection.toArray();
+    
+            userArr.forEach(async (user) => {
+                let userAccessToken = user.access_token;
+                let userRefreshToken = user.refresh_token;
+                let userEmail = user.email;
+    
+                var refresh_token = userRefreshToken;
+                var authOptions = {
+                  url: 'https://accounts.spotify.com/api/token',
+                  headers: { 'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64')) },
+                  form: {
+                    grant_type: 'refresh_token',
+                    refresh_token: refresh_token
+                  },
+                  json: true
+                };
+                let newAccessToken = await getNewToken(authOptions);
+                console.log(newAccessToken);
+    
+                let recentlyPlayedOptions = {
+                    url: `https://api.spotify.com/v1/me/player/recently-played?limit=50`,
+                    headers: { 'Authorization': 'Bearer ' + newAccessToken },
+                    json: true
+                }
+                let currentlyPlayingOptions = {
+                    url: `https://api.spotify.com/v1/me/player/currently-playing`,
+                    headers: {'Authorization': 'Bearer ' + newAccessToken },
+                    json: true
+                }
+                
+                newSongs = await getNewSongs(currentlyPlayingOptions);
+                //console.log(newSongs);
+    
+                await client.db("SpotifyRefresh").collection("users").updateOne({email: userEmail}, {$set: {access_token: newAccessToken}});
+                resolve(newSongs);
+            })
+        }catch(err){
+            console.log(err);
+        }
+    })
+    
 
-    return returnArray;
+    //return newSongs;
 }
-
+async function getNewSongs(options){
+    return new Promise((resolve, reject) => {
+        request.get(options, function(error, response, body){
+            resolve(body);
+        })
+    })
+}
 
 
 async function getNewToken(authOptions){
@@ -75,7 +98,10 @@ app.get('/', (req, res) => {
 });
 
 app.get('/login', async (req, res) => {
-    main().catch(console.error);
+    let songs = await main().catch(console.error);
+    console.log(songs.item.name);
+    //let songNames = songs.items.map(song => song.track.name);
+    //console.log(songNames);
     res.redirect('/')
 })
 
